5af9bfb0ccb0500485a840634328a17f1c7943df
EngineMachiner <mfpc98@gmail.com>
2019-02-27 18:01:56 -0300

    Just scripts. Nothing else.

--- 5th/BGAnimations/ParticleEffects/KaleidoscopeB/Core.lua
+++
@@ -1,9 +0,0 @@
-local ScaleVar = _screen.h/480
-return Def.ActorFrame{
-	LoseFocusCommand=function(self)
-		self:RunCommandsOnChildren(function(child) child:visible(false):finishtweening() end, {})
-	end,
-	LoadActor("A.lua")..{
-		OnCommand=cmd(zoom,0.5*ScaleVar)
-	};
-}
--- 5th/BGAnimations/Scripts/SplitScreen/default.lua
+++ 5th/BGAnimations/Scripts/SplitScreen/default.lua
@@ -1,5 +1,5 @@
 local ScaleVar = _screen.h/480
-local Backgrounds = ...
+local Backgrounds, Reversed = ...
 
 local t = Def.ActorFrame{}
 
@@ -11,17 +11,19 @@ local t = Def.ActorFrame{}
 
     t[#t+1] = LoadActor( Backgrounds[1] )..{
 		OnCommand=function(self)
-			self:Center():SetSize(640*ScaleVar,480*ScaleVar):x(self:GetX()+self:GetWidth()*ScaleVar):zoomx(-ScaleVar)
+			self:Center():SetSize(640*ScaleVar,480*ScaleVar):x(self:GetX()+self:GetWidth()):zoomx(-1)
 		end
 	}
 
    t[#t+1] = LoadActor( Backgrounds[1] )..{
 		OnCommand=function(self)
-			self:Center():SetSize(640*ScaleVar,480*ScaleVar):x(self:GetX()-self:GetWidth()*ScaleVar):zoomx(-ScaleVar)
+			self:Center():SetSize(640*ScaleVar,480*ScaleVar):x(self:GetX()-self:GetWidth()):zoomx(-1)
 		end
 	}
 
 local s_var = math.abs( #Backgrounds - 1 + ( #Backgrounds - 2 ) * 3 )
+
+if not Reversed then
 
 for i = #Backgrounds, 1, -1 do
 	
@@ -29,13 +31,13 @@ for i = #Backgrounds, 1, -1 do
 		OnCommand=function(self)
 			self:Center():SetSize(640*ScaleVar,480*ScaleVar)
 			:diffusealpha(0):cropright(0.5):cropbottom(0.5)
-			:sleep( i * 4 ):linear(2):diffusealpha(1):sleep(2):linear(3):xy(self:GetX()-self:GetWidth()*ScaleVar/2, self:GetY()-self:GetHeight()/2):set_tween_uses_effect_delta(true):effectclock('beat')
+			:sleep( i * 4 ):linear(2):diffusealpha(1):sleep(2):linear(3):xy(self:GetX()-self:GetWidth()/2, self:GetY()-self:GetHeight()/2):set_tween_uses_effect_delta(true):effectclock('beat')
 			:queuecommand("Repeat")
 		end,
 		RepeatCommand=function(self)
 			self:finishtweening():Center():SetSize(640*ScaleVar,480*ScaleVar)
 			:diffusealpha(0):cropright(0.5):cropbottom(0.5)
-			:sleep( s_var ):linear(2):diffusealpha(1):sleep(2):linear(3):xy(self:GetX()-self:GetWidth()*ScaleVar/2, self:GetY()-self:GetHeight()/2):set_tween_uses_effect_delta(true):effectclock('beat')
+			:sleep( s_var ):linear(2):diffusealpha(1):sleep(2):linear(3):xy(self:GetX()-self:GetWidth()/2, self:GetY()-self:GetHeight()/2):set_tween_uses_effect_delta(true):effectclock('beat')
 			:queuecommand("Repeat")
 		end
 	}
@@ -44,13 +46,13 @@ for i = #Backgrounds, 1, -1 do
 		OnCommand=function(self)
 			self:Center():SetSize(640*ScaleVar,480*ScaleVar)
 			:diffusealpha(0):cropleft(0.5):cropbottom(0.5)
-			:sleep( i * 4 ):linear(2):diffusealpha(1):sleep(2):linear(3):xy(self:GetX()+self:GetWidth()*ScaleVar/2, self:GetY()-self:GetHeight()/2):set_tween_uses_effect_delta(true):effectclock('beat')
+			:sleep( i * 4 ):linear(2):diffusealpha(1):sleep(2):linear(3):xy(self:GetX()+self:GetWidth()/2, self:GetY()-self:GetHeight()/2):set_tween_uses_effect_delta(true):effectclock('beat')
 			:queuecommand("Repeat")
 		end,
 		RepeatCommand=function(self)
 			self:finishtweening():Center():SetSize(640*ScaleVar,480*ScaleVar)
 			:diffusealpha(0):cropleft(0.5):cropbottom(0.5)
-			:sleep( s_var ):linear(2):diffusealpha(1):sleep(2):linear(3):xy(self:GetX()+self:GetWidth()*ScaleVar/2, self:GetY()-self:GetHeight()/2):set_tween_uses_effect_delta(true):effectclock('beat')
+			:sleep( s_var ):linear(2):diffusealpha(1):sleep(2):linear(3):xy(self:GetX()+self:GetWidth()/2, self:GetY()-self:GetHeight()/2):set_tween_uses_effect_delta(true):effectclock('beat')
 			:queuecommand("Repeat")
 		end
 	}
@@ -59,13 +61,13 @@ for i = #Backgrounds, 1, -1 do
 		OnCommand=function(self)
 			self:Center():SetSize(640*ScaleVar,480*ScaleVar)
 			:diffusealpha(0):cropright(0.5):croptop(0.5)
-			:sleep( i * 4 ):linear(2):diffusealpha(1):sleep(2):linear(3):xy(self:GetX()-self:GetWidth()*ScaleVar/2, self:GetY()+self:GetHeight()/2):set_tween_uses_effect_delta(true):effectclock('beat')
+			:sleep( i * 4 ):linear(2):diffusealpha(1):sleep(2):linear(3):xy(self:GetX()-self:GetWidth()/2, self:GetY()+self:GetHeight()/2):set_tween_uses_effect_delta(true):effectclock('beat')
 			:queuecommand("Repeat")
 		end,
 		RepeatCommand=function(self)
 			self:finishtweening():Center():SetSize(640*ScaleVar,480*ScaleVar)
 			:diffusealpha(0):cropright(0.5):croptop(0.5)
-			:sleep( s_var ):linear(2):diffusealpha(1):sleep(2):linear(3):xy(self:GetX()-self:GetWidth()*ScaleVar/2, self:GetY()+self:GetHeight()/2):set_tween_uses_effect_delta(true):effectclock('beat')
+			:sleep( s_var ):linear(2):diffusealpha(1):sleep(2):linear(3):xy(self:GetX()-self:GetWidth()/2, self:GetY()+self:GetHeight()/2):set_tween_uses_effect_delta(true):effectclock('beat')
 			:queuecommand("Repeat")
 		end
 	}
@@ -74,16 +76,84 @@ for i = #Backgrounds, 1, -1 do
 		OnCommand=function(self)
 			self:Center():SetSize(640*ScaleVar,480*ScaleVar)
 			:diffusealpha(0):cropleft(0.5):croptop(0.5)
-			:sleep( i * 4 ):linear(2):diffusealpha(1):sleep(2):linear(3):xy(self:GetX()+self:GetWidth()*ScaleVar/2, self:GetY()+self:GetHeight()/2):set_tween_uses_effect_delta(true):effectclock('beat')
+			:sleep( i * 4 ):linear(2):diffusealpha(1):sleep(2):linear(3):xy(self:GetX()+self:GetWidth()/2, self:GetY()+self:GetHeight()/2):set_tween_uses_effect_delta(true):effectclock('beat')
 			:queuecommand("Repeat")
 		end,
 		RepeatCommand=function(self)
 			self:finishtweening():Center():SetSize(640*ScaleVar,480*ScaleVar)
 			:diffusealpha(0):cropleft(0.5):croptop(0.5)
-			:sleep( s_var ):linear(2):diffusealpha(1):sleep(2):linear(3):xy(self:GetX()+self:GetWidth()*ScaleVar/2, self:GetY()+self:GetHeight()/2):set_tween_uses_effect_delta(true):effectclock('beat')
+			:sleep( s_var ):linear(2):diffusealpha(1):sleep(2):linear(3):xy(self:GetX()+self:GetWidth()/2, self:GetY()+self:GetHeight()/2):set_tween_uses_effect_delta(true):effectclock('beat')
 			:queuecommand("Repeat")
 		end
 	}
+
+end
+
+else
+
+for i = 1, #Backgrounds do
+	
+    t[#t+1] = LoadActor(Backgrounds[i])..{
+		OnCommand=function(self)
+			self:xy(SCREEN_CENTER_X-self:GetWidth()/2, SCREEN_CENTER_Y-self:GetHeight()/2):SetSize(640*ScaleVar,480*ScaleVar)
+			:diffusealpha(1):cropright(0.5):cropbottom(0.5)
+			:sleep(2):linear(3):Center():linear(2):sleep( i * 4 ):diffusealpha(0):set_tween_uses_effect_delta(true):effectclock('beat')
+			:queuecommand("Repeat")
+		end,
+		RepeatCommand=function(self)
+			self:finishtweening():xy(SCREEN_CENTER_X-self:GetWidth()/2, SCREEN_CENTER_Y-self:GetHeight()/2):SetSize(640*ScaleVar,480*ScaleVar)
+			:diffusealpha(1):cropright(0.5):cropbottom(0.5)
+			:sleep(2):linear(3):Center():linear(2):diffusealpha(0):sleep( s_var ):set_tween_uses_effect_delta(true):effectclock('beat')
+			:queuecommand("Repeat")
+		end
+	}
+
+   	t[#t+1] = LoadActor(Backgrounds[i])..{
+		OnCommand=function(self)
+			self:xy(SCREEN_CENTER_X+self:GetWidth()/2, SCREEN_CENTER_Y-self:GetHeight()/2):SetSize(640*ScaleVar,480*ScaleVar)
+			:diffusealpha(1):cropleft(0.5):cropbottom(0.5)
+			:sleep(2):linear(3):Center():linear(2):diffusealpha(0):sleep( i * 4 ):set_tween_uses_effect_delta(true):effectclock('beat')
+			:queuecommand("Repeat")
+		end,
+		RepeatCommand=function(self)
+			self:finishtweening():xy(SCREEN_CENTER_X+self:GetWidth()/2, SCREEN_CENTER_Y-self:GetHeight()/2):SetSize(640*ScaleVar,480*ScaleVar)
+			:diffusealpha(1):cropleft(0.5):cropbottom(0.5)
+			:sleep(2):linear(3):Center():linear(2):diffusealpha(0):sleep( s_var ):set_tween_uses_effect_delta(true):effectclock('beat')
+			:queuecommand("Repeat")
+		end
+	}
+
+   	t[#t+1] = LoadActor(Backgrounds[i])..{
+		OnCommand=function(self)
+			self:xy(SCREEN_CENTER_X-self:GetWidth()/2, SCREEN_CENTER_Y+self:GetHeight()/2):SetSize(640*ScaleVar,480*ScaleVar)
+			:diffusealpha(1):cropright(0.5):croptop(0.5)
+			:sleep(2):linear(3):Center():linear(2):diffusealpha(0):sleep( i * 4 ):set_tween_uses_effect_delta(true):effectclock('beat')
+			:queuecommand("Repeat")
+		end,
+		RepeatCommand=function(self)
+			self:finishtweening():xy(SCREEN_CENTER_X-self:GetWidth()/2, SCREEN_CENTER_Y+self:GetHeight()/2):SetSize(640*ScaleVar,480*ScaleVar)
+			:diffusealpha(1):cropright(0.5):croptop(0.5)
+			:sleep(2):linear(3):Center():linear(2):diffusealpha(0):sleep( s_var ):set_tween_uses_effect_delta(true):effectclock('beat')
+			:queuecommand("Repeat")
+		end
+	}
+
+   	t[#t+1] = LoadActor(Backgrounds[i])..{
+		OnCommand=function(self)
+			self:xy(SCREEN_CENTER_X+self:GetWidth()/2, SCREEN_CENTER_Y+self:GetHeight()/2):SetSize(640*ScaleVar,480*ScaleVar)
+			:diffusealpha(1):cropleft(0.5):croptop(0.5)
+			:sleep(2):linear(3):Center():linear(2):diffusealpha(0):sleep( i * 4 ):set_tween_uses_effect_delta(true):effectclock('beat')
+			:queuecommand("Repeat")
+		end,
+		RepeatCommand=function(self)
+			self:finishtweening():xy(SCREEN_CENTER_X+self:GetWidth()/2, SCREEN_CENTER_Y+self:GetHeight()/2):SetSize(640*ScaleVar,480*ScaleVar)
+			:diffusealpha(1):cropleft(0.5):croptop(0.5)
+			:sleep(2):linear(3):Center():linear(2):diffusealpha(0):sleep( s_var ):set_tween_uses_effect_delta(true):effectclock('beat')
+			:queuecommand("Repeat")
+		end
+	}
+
+end
 
 end
 
