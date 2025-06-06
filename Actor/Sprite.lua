09c4b45b3e4a833129a816adbf7ce4394163e9b9
EngineMachiner <mfpc98@gmail.com>
2025-06-06 18:43:25 -0500

    Update Sprite.lua

--- Actor/Sprite.lua
+++ Actor/Sprite.lua
@@ -5,6 +5,8 @@ local astro = Astro.Table
 local function hasAnimationType( self, name ) return self.beat4sprite:hasAnimationType(name) end
 
 
+-- TODO: Use tapLua.spriteMatrix
+
 local function pathMatrix(self)
 
     local path = self:GetTexture():GetPath()        if not FILEMAN:DoesFileExist(path) then return end    
